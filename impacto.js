75da6a1549cf53119dc1576a80f51a4c8f372729
JSCEG <137372117+JSCEG@users.noreply.github.com>
2025-08-25 20:33:06 -0600

    Improve quartile highlights and normalization

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418e586388bc6d3dbfb0c07e12fb93d31d7c6e7ccaf8ddc044f63ac85d13ca88
-size 36303
+oid sha256:ae1188fe60c18d0bc96a1b440f0d9bcd5408041c4fa7a8a21786e696be1e3b46
+size 36337
