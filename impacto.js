517dbee3e83e02a00513e3ab582de41608c2e242
Tu Nombre <tu@correo.com>
2025-08-26 17:04:15 -0600

    Avance 10 26082025

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c58c6df6f41aa3bb27ae462f9b96eaf35cb567dc78fc24ae3476931072daaae1
-size 107941
+oid sha256:075f43be5627d39d57b2d775d1d4dbc35d32c510f6db65e33934a96c3fb667d5
+size 111579
