2725a5635d717607f4c01f532aa9d9e71898952b
Tu Nombre <tu@correo.com>
2025-08-25 20:55:08 -0600

    highlights

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418e586388bc6d3dbfb0c07e12fb93d31d7c6e7ccaf8ddc044f63ac85d13ca88
-size 36303
+oid sha256:5d431c7fec7eb59c97c59bd21da9bb8568d324c3031f49071d4cf835788b4bb8
+size 36460
