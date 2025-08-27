ed991e51a8281fedf576c0117a25286671d81d87
Tu Nombre <tu@correo.com>
2025-08-27 13:55:43 -0600

    Avance 5 a excel mexcio 27082025

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6094e0bc56f4848d3ab5b63fa97e3f7035f3c2d5b9999b5b00ea2b9f1fd470cd
-size 131397
+oid sha256:487ff080926276a9f8bb1fc7c1c4448862a827c44829576744f0ca09dc4d1b64
+size 132701
