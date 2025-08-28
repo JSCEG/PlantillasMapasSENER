81aad18841e633b5f251186c13f20a15fda5f285
Tu Nombre <tu@correo.com>
2025-08-27 19:42:37 -0600

    Avance entidad 27082025

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0445dfdbee57357aa4b9108738940a46320e4440085b83b105fca304ac8e0b33
-size 146762
+oid sha256:42c5c6fb8e6e6be872313163da9caec719ead5fc1be8fd53e7d73f38fb5d375c
+size 149361
--- mapa.js
+++ mapa.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b0bcd2ce23e3017a06868a1875ebfe09d4d124302742cbd037ca9012dcdd476
-size 61609
+oid sha256:1ec47c684b772ed1e7b9b941a2c671e5b82e047693598f151e21d6d6fb2a3938
+size 68275
