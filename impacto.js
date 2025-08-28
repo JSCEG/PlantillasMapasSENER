9d6429f9e7e7375592550339ca87828369a0afad
Tu Nombre <tu@correo.com>
2025-08-27 18:00:16 -0600

    Avance 7 capas 27082025

--- impacto.js
+++ impacto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99701d80f4167e3eb62bb262d5f3305115b6c5ecb4aafff5f277b9d67a7e656e
-size 145441
+oid sha256:0445dfdbee57357aa4b9108738940a46320e4440085b83b105fca304ac8e0b33
+size 146762
--- mapa.js
+++ mapa.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c325996f1c1287506233f31e66507704cbe1fc0cc67c57b7cc3dfe6a88fd443
-size 38428
+oid sha256:0ce9f287d9a57b2fc9e9597c9d42a69b39b18e453cbe4d2d3efc1ed06ba16e52
+size 62340
